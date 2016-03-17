3ba5e30a55545de830d49b9bcf481bcf0f14b08a
Sunchy321 <sunchy321@qq.com>
2016-03-17 15:05:00 +0800

    init

--- src/parser/TrigraphParser.js
+++ src/parser/TrigraphParser.js
@@ -4,14 +4,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -52,7 +52,7 @@ export default class TrigraphParser {
 			if (content[i] === '?' && content[i + 1] === '?') {
 				if (content[i + 2] in trigraphMap) {
 					context.emitDiagnostics(new DiagnosticMessage(DiagnosticMessage.LEVEL_WARNING, 'Use of trigraph is discouraged', source.range(i, i + 2, i + 3)));
-					
+
 					if (!builder) {
 						mapping = new IndexMap();
 					}
--- src/pass/TrailingSpacePass.js
+++ src/pass/TrailingSpacePass.js
@@ -4,14 +4,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -37,7 +37,7 @@ export default class TrailingSpacePass {
 		while ((match = matcher.exec(content)) != null) {
 			let len = match[0].length - 1;
 			context.emitDiagnostics(
-				new DiagnosticMessage(DiagnosticMessage.LEVEL_WARNING, 
+				new DiagnosticMessage(DiagnosticMessage.LEVEL_WARNING,
 					len === 1 ? 'Trailing space is discouraged' : 'Trailing spaces are discouraged',
 					source.range(match.index, match.index + len)));
 		}
